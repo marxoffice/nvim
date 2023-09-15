dd4b97cf5781150a3bde7af57cfab15878469a1b
Rayquaza <marxoffice@foxmail.com>
2023-09-15 17:43:26 +0800

    Fix: tab as space

--- init.lua
+++ init.lua
@@ -12,7 +12,7 @@ vim.opt.wrap = true
 vim.opt.breakindent = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
-vim.opt.expandtab = false
+vim.opt.expandtab = true
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.whichwrap:append("<,>,[,]")
